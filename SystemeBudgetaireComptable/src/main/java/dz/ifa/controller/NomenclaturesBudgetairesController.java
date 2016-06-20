a82b0c729eb362d683289f9d786de934c9862cc5
Yazid <cb_medjdoub@esi.dz>
2016-06-20 15:31:04 +0100

    906 ---> 909

--- SystemeBudgetaireComptable/src/main/java/dz/ifa/controller/NomenclaturesBudgetairesController.java
+++ SystemeBudgetaireComptable/src/main/java/dz/ifa/controller/NomenclaturesBudgetairesController.java
@@ -75,7 +75,7 @@ public class NomenclaturesBudgetairesController {
 
         if (service.getNomenclatureComptableByNumeroNomenclature(code_strcuture).size() > 0) {
             System.out.println("Strcuture Exist");
-            return "902";
+            return "909";
         }
         Structure structure=new Structure();
         structure.setAdresse(address_strcuture);
